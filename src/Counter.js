e24eeb6aaa82ee4407fbb73b5259aa7a43a105a5
kubzer0 <jakub.gryka@gmail.com>
2018-11-28 11:40:53 +0100

    seting state in database

--- src/Counter.js
+++ src/Counter.js
@@ -13,25 +13,39 @@ class Counter extends React.Component {
         this.setState({ number: this.state.number - 1 })
     }
 
-    render() {
-        return (
-            <div>
-                <h1>{this.state.number}</h1>
-                <button
-                    onClick={this.incHandler}
-                >
-                    +
-            </button>
-                <button
-                    onClick={this.decHandler}
-                >
-                    -
-            </button>
-
-
-            </div>
+    saveToFirebase = () =>{
+        fetch('https://poniedzialek-5c108.firebaseio.com/counter.json',
+        {
+            method : 'PUT',
+            body: JSON.stringify(this.state.number)
+        }
         )
     }
+
+    componentDidUpdate(){
+        this.saveToFirebase()
+    }
+
+
+        render() {
+    return (
+        <div>
+            <h1>{this.state.number}</h1>
+            <button
+                onClick={this.incHandler}
+            >
+                +
+            </button>
+            <button
+                onClick={this.decHandler}
+            >
+                -
+            </button>
+
+
+        </div>
+    )
+}
 }
 
 export default Counter
