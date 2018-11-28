5613fedeb108810a857388852c2c54b04d85804a
kubzer0 <jakub.gryka@gmail.com>
2018-11-28 12:34:42 +0100

    usin databes.ref().on

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { database } from './firebaseConfig'
 
 class Counter extends React.Component {
+
     state = {
         number: 0
+    }
+
+    componentDidMount() {
+        database.ref('/counter').on(
+            'value',
+            snapshot => {
+                this.setState({ number: snapshot.val() })
+            }
+        )
     }
 
     incHandler = () => {
@@ -13,49 +24,25 @@ class Counter extends React.Component {
         this.setState({ number: this.state.number - 1 })
     }
 
-    saveToFirebase = () =>{
-        fetch('https://poniedzialek-5c108.firebaseio.com/counter.json',
-        {
-            method : 'PUT',
-            body: JSON.stringify(this.state.number)
-        }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.number}</h1>
+                <button
+                    onClick={this.incHandler}
+                >
+                    +
+            </button>
+                <button
+                    onClick={this.decHandler}
+                >
+                    -
+            </button>
+
+
+            </div>
         )
     }
-
-    readFromFirebase=()=>{
-        fetch('https://poniedzialek-5c108.firebaseio.com/counter.json')
-        .then(response=> response.json())
-        .then(data => this.setState({number: data}))
-    }
-
-    componentDidMount(){
-        this.readFromFirebase()
-    }
-
-    componentDidUpdate(){
-        this.saveToFirebase()
-    }
-
-
-        render() {
-    return (
-        <div>
-            <h1>{this.state.number}</h1>
-            <button
-                onClick={this.incHandler}
-            >
-                +
-            </button>
-            <button
-                onClick={this.decHandler}
-            >
-                -
-            </button>
-
-
-        </div>
-    )
-}
 }
 
 export default Counter
