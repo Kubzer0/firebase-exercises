99a650bf855a5639155006fa68f576dc6813e48b
kubzer0 <jakub.gryka@gmail.com>
2018-11-28 11:17:36 +0100

    firebase config added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+  var config = {
+    apiKey: "AIzaSyDa1r8ZYtaRJxf2lQUXorbnynYov8U1gzY",
+    authDomain: "poniedzialek-5c108.firebaseapp.com",
+    databaseURL: "https://poniedzialek-5c108.firebaseio.com",
+    projectId: "poniedzialek-5c108",
+    storageBucket: "poniedzialek-5c108.appspot.com",
+    messagingSenderId: "565385340536"
+  }
+  firebase.initializeApp(config)
