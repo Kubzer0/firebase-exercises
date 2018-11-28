ff31feba72e0f6d9926a1bab9abab563db125702
kubzer0 <jakub.gryka@gmail.com>
2018-11-28 11:28:45 +0100

    counter component added

---
+++ src/Counter.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+class Counter extends React.Component {
+    state = {
+        number: 0
+    }
+
+    incHandler = () => {
+        this.setState({ number: this.state.number + 1 })
+    }
+
+    decHandler = () => {
+        this.setState({ number: this.state.number - 1 })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.number}</h1>
+                <button
+                    onClick={this.incHandler}
+                >
+                    +
+            </button>
+                <button
+                    onClick={this.decHandler}
+                >
+                    -
+            </button>
+
+
+            </div>
+        )
+    }
+}
+
+export default Counter
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,13 @@
 import firebase from 'firebase'
 
-  var config = {
+var config = {
     apiKey: "AIzaSyDa1r8ZYtaRJxf2lQUXorbnynYov8U1gzY",
     authDomain: "poniedzialek-5c108.firebaseapp.com",
     databaseURL: "https://poniedzialek-5c108.firebaseio.com",
     projectId: "poniedzialek-5c108",
     storageBucket: "poniedzialek-5c108.appspot.com",
     messagingSenderId: "565385340536"
-  }
-  firebase.initializeApp(config)
+}
+firebase.initializeApp(config)
+
+export const database = firebase.database()
